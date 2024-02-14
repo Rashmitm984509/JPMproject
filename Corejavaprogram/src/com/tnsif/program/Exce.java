aefb93809610f3ab39c0330c1b969a79891f5707
TNS INDIA <TNS INDIA@192.168.1.22>
2024-02-14 15:16:58 +0530

    First batch

---
+++ Corejavaprogram/src/com/tnsif/program/Exce.java
@@ -0,0 +1,11 @@
+package com.tnsif.program;
+
+public class Exce {
+public static void main(String[] args) {
+	Encapsulation e=new Encapsulation();
+	e.serialnumber=6;
+	e.name="suma";
+	e.age=23;
+	e.show();
+}
+}
