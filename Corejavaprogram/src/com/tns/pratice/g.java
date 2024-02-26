413a043867ae35f0a62a11e988825a61e93c8679
TNS INDIA <TNS INDIA@192.168.1.26>
2024-02-26 21:50:57 +0530

    First batch

---
+++ Corejavaprogram/src/com/tns/pratice/g.java
@@ -0,0 +1,12 @@
+package com.tns.pratice;
+
+public class g {
+public static void main(String[] args) {
+	StringBuffer s=new StringBuffer("welcome");
+	//s.append("welcome");
+	//s.insert(2, "hi");
+	//s.delete(2, 4);
+	//s.reverse();
+	s.capacity();
+	System.out.println(s);
+}}
